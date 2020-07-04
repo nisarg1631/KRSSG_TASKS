25fd9c2beddec76615b32155a715efb6e6b1ff61
Nisarg Upadhyaya <52166493+nisarg1631@users.noreply.github.com>
2020-07-04 15:41:22 +0530

    Update rect_fsm.py

--- rect_fsm.py
+++ rect_fsm.py
@@ -10,55 +10,102 @@ from math import *
 class Rectangle(behavior.Behavior):
 
     class State(Enum):
-        BOT_STATIC = 1
-        BOT_RECEIVE = 2
-        BOT_PASSING = 3
-        BOT_ALLIGNING = 4
+        setup = 1
+        static = 2
+        receive = 3
+        passing = 4
+        alligning = 5
     
-    def __init__(self, continuous = False):
+    def __init__(self, id, next_id, prev_id, pos, continuous = False):
 
         super(Rectangle,self).__init__()
 
-        self.name = "Rectangle"
+        self.name = "Rectangle" + str(id)
+        self.id = id
+        self.next_id = next_id
+        self.prev_id = prev_id
+        self.pos = pos
         
         self.behavior_failed = False
 
-        self.add_state(Rectangle.State.BOT_STATIC,
-            behavior.Behavior.State.running)
+        for state in Rectangle.State:
+            self.add_state(state,behavior.Behavior.State.running)
+        
+        self.add_transition(behavior.Behavior.State.start, Rectangle.State.setup,
+            lambda: True,"initialize")
+        
+        self.add_transition(Rectangle.State.setup, Rectangle.State.receive,
+            lambda: self.prev_has_ball(),"receive_pass")
+        self.add_transition(Rectangle.State.setup, Rectangle.State.passing,
+            lambda: self.has_ball(),"pass_to_next")
+        self.add_transition(Rectangle.State.setup, Rectangle.State.alligning,
+            lambda: (not self.prev_has_ball()) and (not self.has_ball()) and (not self.at_pos()),"move_to_pos")
+        self.add_transition(Rectangle.State.setup, Rectangle.State.static,
+            lambda: (not self.prev_has_ball()) and (not self.has_ball()) and self.at_pos(),"stay_at_pos")
+        
+        self.add_transition(Rectangle.State.static, Rectangle.State.alligning,
+            lambda: (not self.prev_has_ball()) and (not self.has_ball()) and (not self.at_pos()),"allign")
+        self.add_transition(Rectangle.State.alligning, Rectangle.State.static,
+            lambda: self.at_pos(),"stay")
+        self.add_transition(Rectangle.State.static, Rectangle.State.receive,
+            lambda: self.prev_has_ball(),"receive")
+        self.add_transition(Rectangle.State.static, Rectangle.State.passing,
+            lambda: self.has_ball(),"receive")
+        self.add_transition(Rectangle.State.receive, Rectangle.State.passing,
+            lambda: self.has_ball(),"pass")
+        self.add_transition(Rectangle.State.passing, Rectangle.State.alligning,
+            lambda: not self.at_pos(),"allign")
+        self.add_transition(Rectangle.State.passing, Rectangle.State.static,
+            lambda: self.at_pos(),"stay")
 
-        self.add_state(Rectangle.State.BOT_RECEIVE,
-            behavior.Behavior.State.running)
+        for state in Rectangle.State:
+            self.add_transition(state,Rectangle.State.setup,
+                lambda: self.behavior_failed,"failed")
 
-        self.add_state(Rectangle.State.BOT_PASSING,
-            behavior.Behavior.State.running)
-
-        self.add_state(Rectangle.State.BOT_ALLIGNING,
-            behavior.Behavior.State.running)
-        
-        self.add_transition(behavior.Behavior.State.start,
-            Rectangle.State.BOT_STATIC,lambda: True,'initialize')
-        
-        self.add_transition(Rectangle.State.BOT_STATIC,
-            Rectangle.State.BOT_RECEIVE,lambda: ,'receive_pass')
-        
-        self.add_transition(Rectangle.State.BOT_STATIC,
-            Rectangle.State.BOT_PASSING,lambda: ,'pass_to_next')
-
-        self.add_transition(Rectangle.State.BOT_STATIC,
-            Rectangle.State.BOT_ALLIGNING,lambda: ,'move_to_pos')
-        
-        self.add_transition(Rectangle.State.BOT_RECEIVE,
-            Rectangle.State.BOT_STATIC,lambda: ,'pass_received')
-        
-        self.add_transition(Rectangle.State.BOT_PASSING,
-            Rectangle.State.BOT_STATIC,lambda: ,'ball_passed')
-        
-        self.add_transition(Rectangle.State.BOT_ALLIGNING,
-            Rectangle.State.BOT_STATIC,lambda: ,'at_pos')
-
-    def add_kub(self, kub, pos, previous, target):
+    def add_kub(self, kub):
         self.kub = kub
-        self.pos = pos
-        self.previous = previous
-        self.target = target
     
+    def add_theta(self, theta):
+	    self.theta = theta
+
+    def prev_has_ball(self):
+        pass
+    def has_ball(self):
+        pass
+    def at_pos(self):
+        pass
+    
+    def on_enter_setup(self):
+        pass
+    def execute_setup(self):
+        pass
+    def on_exit_setup(self):
+        pass
+
+    def on_enter_static(self):
+        pass
+    def execute_static(self):
+        pass
+    def on_exit_static(self):
+        pass
+
+    def on_enter_alligning(self):
+        pass
+    def execute_alligning(self):
+        pass
+    def on_exit_alligning(self):
+        pass
+
+    def on_enter_receive(self):
+        pass
+    def execute_receive(self):
+        pass
+    def on_exit_receive(self):
+        pass
+
+    def on_enter_passing(self):
+        pass
+    def execute_passing(self):
+        pass
+    def on_exit_passing(self):
+        pass
